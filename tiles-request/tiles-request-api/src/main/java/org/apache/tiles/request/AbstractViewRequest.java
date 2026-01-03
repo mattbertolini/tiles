ee8cfa70a6cd85828459d10e3b011ce6743cc0cd
Matt Bertolini <viper2843@gmail.com>
2026-01-03 10:47:40 -0500

    Mark AbstractViewRequest as abstract

--- tiles-request/tiles-request-api/src/main/java/org/apache/tiles/request/AbstractViewRequest.java
+++ tiles-request/tiles-request-api/src/main/java/org/apache/tiles/request/AbstractViewRequest.java
@@ -30,7 +30,7 @@ import java.io.IOException;
  *
  * @version $Rev$ $Date$
  */
-public class AbstractViewRequest extends DispatchRequestWrapper {
+public abstract class AbstractViewRequest extends DispatchRequestWrapper {
 
     /**
      * Constructor.
